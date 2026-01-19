2b53d2c7eceb69c3b1231051184da6aa7e3c4e1d
N9K <nelcome9@gmail.com>
2026-01-19 14:33:22 +0000

    initial commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { BookOpen, Menu, Search } from 'lucide-react';
+import { BookOpen, Menu } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 
 import {
